92e498834ab9199885843cd76d9dbc78d9b6da0e
pb-wd027 <pbauer@protonmail.com>
2022-07-13 16:57:03 +0200

    app.js some modifications

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
